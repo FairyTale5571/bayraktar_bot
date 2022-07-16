70737f34a21b5c2540eb758363b8d0db9516995c
FairyTale#5571 <onopa96@gmail.com>
2022-07-16 13:57:43 +0300

    graceful shutdown

--- cmd/bayraktar_bot/main.go
+++ cmd/bayraktar_bot/main.go
@@ -1,18 +1,29 @@
 package main
 
 import (
-	"github.com/fairytale5571/bayraktar_bot/pkg/app"
 	"log"
 	"os"
 	"os/signal"
+
+	"github.com/fairytale5571/bayraktar_bot/pkg/app"
 )
 
 func main() {
-	if err := app.New(); err != nil {
+	app, err := app.New()
+	if err != nil {
 		log.Fatalf("start application failed: %v", err)
 	}
 	stop := make(chan os.Signal)
 	signal.Notify(stop, os.Interrupt)
-	<-stop
+	sig := <-stop
+	app.Logger.Infof("Close: received %v", sig.String())
+
+	err = app.DB.Close()
+	if err != nil {
+		app.Logger.Errorf("Close: error close database: %v", err)
+		return
+	}
+	app.Discord.Stop()
+
 	log.Fatalf("Graceful shutdown\n************************************************************************\n\n")
 }
