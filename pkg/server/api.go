311796f9a1b0996a5a0eb6b1dfd80a318700bffd
FairyTale#5571 <onopa96@gmail.com>
2022-08-25 01:36:00 +0300

    influenced resource

--- pkg/server/api.go
+++ pkg/server/api.go
@@ -24,12 +24,13 @@ func (r *Router) economy(c *gin.Context) {
 		RandomMax        int
 		RandomMin        int
 		Illegal          bool
+		Influenced       string
 		LastUpdate       time.Time
 	}
 	var economies []Economy
 	for rows.Next() {
 		var e Economy
-		err := rows.Scan(&e.Resource, &e.Localize, &e.Price, &e.MaxPrice, &e.MinPrice, &e.DownPricePerItem, &e.RandomDownPrice, &e.RandomMax, &e.RandomMin, &e.Illegal, &e.LastUpdate)
+		err := rows.Scan(&e.Resource, &e.Localize, &e.Price, &e.MaxPrice, &e.MinPrice, &e.DownPricePerItem, &e.RandomDownPrice, &e.RandomMax, &e.RandomMin, &e.Illegal, &e.Influenced, &e.LastUpdate)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
