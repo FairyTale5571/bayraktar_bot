e67948bc54d14c671ba844a3b334dcffa0ee1671
FairyTale#5571 <onopa96@gmail.com>
2022-09-10 22:52:39 +0300

    caching module

---
+++ pkg/cache/redis_cache/redis_cache.go
@@ -0,0 +1,40 @@
+package redis_cache
+
+import (
+	"time"
+
+	"github.com/fairytale5571/bayraktar_bot/pkg/storage"
+	"github.com/fairytale5571/bayraktar_bot/pkg/storage/redis"
+)
+
+type RedisCache struct {
+	rdb *redis.Redis
+}
+
+func New(rdb *redis.Redis) *RedisCache {
+	return &RedisCache{
+		rdb: rdb,
+	}
+}
+
+func (c *RedisCache) Set(key, value string, ttl time.Duration) error {
+	if err := c.rdb.SetTTL(key, value, storage.Cache, ttl); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *RedisCache) Get(key string) (string, error) {
+	res, err := c.rdb.Get(key, storage.Cache)
+	if err != nil {
+		return "", err
+	}
+	return res, nil
+}
+
+func (c *RedisCache) Delete(key string) {
+	c.rdb.Delete(key, storage.Cache)
+}
+
+func (c *RedisCache) Cleanup() {
+}
--- pkg/errorUtils/errorUtils.go
+++ pkg/errorUtils/errorUtils.go
@@ -7,4 +7,10 @@ var (
 	ErrSteamUserNotFound = errors.New("steam user not found")
 
 	ErrTicketOpened = errors.New("ticket already opened")
+
+	ErrorNotCached        = errors.New("not cached")
+	ErrorCantCacheRedis   = errors.New("can't cache redis")
+	ErrorCantCacheMemory  = errors.New("can't cache memory")
+	ErrorCantDeleteMemory = errors.New("can't delete memory")
+	ErrorCantDeleteRedis  = errors.New("can't delete redis")
 )
